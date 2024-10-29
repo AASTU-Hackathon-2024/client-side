8b846d44d4f8c9195a4ed2cddf773202f0a3a0be
Temesgen <temesgenselam1995@gmail.com>
2024-10-30 00:32:24 +0300

    fetari erdan

--- src/pages/sign-up.jsx
+++ src/pages/sign-up.jsx
@@ -141,9 +141,7 @@ export default function SignUp() {
         </h2>
       </div>
 
-      <button onClick={test} className="text-3xl">
-        test
-      </button>
+      <p className="text-3xl text-center">👋</p>
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
